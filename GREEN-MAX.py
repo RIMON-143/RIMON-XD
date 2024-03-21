c6267fa1cebdf1d2ca56b54c543dffd3276527a0
RIMON-AHMED <100198777+RIMON-143@users.noreply.github.com>
2024-03-21 18:06:18 +0600

    Update GREEN-MAX.py

--- GREEN-MAX.py
+++ GREEN-MAX.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
 # exit(' Wait Tool On updating ')
-Brand=platform.architecture()[0]
+Green=platform.architecture()[0]
 if Green=="32bit":__import__("Green32")
 elif Green=="64bit":__import__("Green64")
